82b2cf51bc759a2e913ec81ef044399ace1aa065
Simon D. Levy <simon.d.levy@gmail.com>
2026-07-01 14:58:05 -0400

    =Cleanup

--- levy/conn/__init__.py
+++ levy/conn/__init__.py
@@ -136,7 +136,7 @@ class InpConfig(_IoConfig):
 
     def _get_charge_width(self):
         # TODO: support fires input
-        return self._charge_width # charge_width(self._network)
+        return self._charge_width
 
 
 class OutConfig(_IoConfig):
