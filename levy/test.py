8706a1ed3f011927c35001cd655d3779db35439e
Simon D. Levy <simon.d.levy@gmail.com>
2026-07-01 00:46:00 -0400

    Cleanup

--- levy/test.py
+++ levy/test.py
@@ -16,12 +16,8 @@ for _ in range(3):
 
     conn.clear_activity()
 
-    # proc.apply_spikes([neuro.Spike(0, i, 1.0) for i in range(3)])
     conn.apply_spikes([neuro.Spike(0, i, 1.0) for i in range(3)])
 
-    # proc.run(6)
     conn.run(6)
 
-    # print(proc.output_last_fire(0))
     print(conn.output_last_fire(0))
-    #print()
