69d85f1f6bbb92c9f0d9181cd3ac10ea616be098
Simon D. Levy <simon.d.levy@gmail.com>
2026-06-30 21:04:08 -0400

    modified:   conn/__init__.py

--- levy/conn/__init__.py
+++ levy/conn/__init__.py
@@ -1,3 +1,5 @@
+import neuro
+
 from periphery import Serial
 
 class Connection:
@@ -13,3 +15,18 @@ class Connection:
  
         pass
 
+    def clear_activity(self) -> None:
+
+        pass
+
+    def apply_spikes(self, spikes: list[neuro.Spike]) -> None:
+
+        pass
+
+    def run(self, time: int) -> None:
+
+        pass
+
+    def output_last_fire(self, out_idx: int) -> float:
+
+        return 0
--- levy/test.py
+++ levy/test.py
@@ -11,7 +11,16 @@ proc.load_network(net)
 conn = conn.Connection("basys3", "/dev/ttyUSB1", "DIDO")
 
 for _ in range(3):
+
     proc.clear_activity()
+    conn.clear_activity()
+
     proc.apply_spikes([neuro.Spike(0, i, 1.0) for i in range(3)])
+    conn.apply_spikes([neuro.Spike(0, i, 1.0) for i in range(3)])
+
     proc.run(6)
+    conn.run(6)
+
     print(proc.output_last_fire(0))
+    print(conn.output_last_fire(0))
+    print()
