4bbf4b26b0899b180bbb4b48bb618d957e1c41c2
Simon D. Levy <simon.d.levy@gmail.com>
2026-09-05 15:59:52 -0400

    new file:   xor_load_cmod.py, xor_test_cmod.py

---
+++ examples/xor_test_cmod.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+
+# Copyright (c) 2026 Simon D. Levy
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+import neuro
+import fpga
+
+net = neuro.Network()
+net.read_from_file("../networks/xor.txt")
+
+proc = fpga.Processor("cmod", "/dev/ttyUSB1", "DIDO")
+
+proc.attach_network(net)
+
+def run(a, b):
+    proc.clear_activity()
+    if a:
+        proc.apply_spike(neuro.Spike(0, 0, 1))
+    if b:
+        proc.apply_spike(neuro.Spike(1, 0, 1))
+    proc.run(3)
+    print('input = %d,%d; output = %d' % (a, b, proc.output_counts()[0]))
+
+
+run(0, 0)
+run(0, 1)
+run(1, 0)
+run(1, 1)
