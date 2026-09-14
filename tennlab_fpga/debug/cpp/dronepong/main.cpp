66fa411164d39721bad645863f3b87f4e1f02665
Simon D. Levy <simon.d.levy@gmail.com>
2026-09-14 00:10:36 -0400

    modified:   main.cpp

--- tennlab_fpga/debug/cpp/dronepong/main.cpp
+++ tennlab_fpga/debug/cpp/dronepong/main.cpp
@@ -7,12 +7,7 @@
  */
 
 #include <stdio.h>
-
-#include <iostream>
-#include <fstream>
-#include <sstream>
 #include <string>
-#include <vector>
 
 #include <processor.hpp>
 
@@ -20,83 +15,6 @@
 #include "spikes.h"
 
 static const std::string kPortName = "/dev/ttyUSB1";
-static const std::string kTestData = "spikes.txt";
-
-/*
-class Entry {
-
-    public:
-
-        int step;
-        int id;
-        float time;
-        float value;
-
-        Entry(
-                const int step,
-                const int id,
-                const float time,
-                const float value)
-            : step(step), id(id), time(time), value(value) {}
-
-        Entry() = default;
-
-        Entry(const Entry & other) = default;
-};
-
-static auto parse(std::stringstream & ss) -> std::string
-{
-    std::string token;
-    std::getline(ss, token, ' ');
-    return token;
-}
-
-static auto parseint(std::stringstream & ss) -> int
-{
-    return std::stoi(parse(ss));
-}
-
-static auto parsefloat(std::stringstream & ss) -> float
-{
-    return std::stof(parse(ss));
-}
-
-static auto loaddata() -> std::vector<Entry>
-{
-    std::ifstream file(kTestData);
-
-    std::vector<Entry> data;             
-
-    if (!file.is_open()) {
-        std::cerr << "Error opening file " << kTestData << std::endl;
-        exit(1);
-    }
-
-    while (true) {
-
-        std::string line;
-
-        if (!std::getline(file, line)) {
-            break;
-        }
-
-        std::stringstream ss(line);
-
-        std::string token;
-
-        const auto step = parseint(ss);
-        const auto id = parseint(ss);
-        const auto time = parsefloat(ss);
-        const auto value = parsefloat(ss);
-
-        data.push_back(Entry(step, id, time, value));
-    }
-
-    file.close();
-
-    return data;
-}
-*/
 
 extern void clear_encoded_spikes();
 extern void encode();
@@ -110,9 +28,7 @@ int main()
 {
     proc_.Connect();
 
-    //const auto data = loaddata(); 
-
-    const auto runtime = entries[kEntries-1].step;//data.back().step + 1;
+    const auto runtime = entries[kEntries-1].step;
 
     for (int timestep=0; timestep<runtime; ++timestep) {
 
