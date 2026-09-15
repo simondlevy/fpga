555a921fdef47cfc32fa3dcc90902df9bd81c170
Simon D. Levy <simon.d.levy@gmail.com>
2026-09-15 02:53:02 -0400

    modified:   ../utils/mkhdr.py

--- tennlab_fpga/utils/mkhdr.py
+++ tennlab_fpga/utils/mkhdr.py
@@ -38,8 +38,7 @@ opcode_width = unsigned_width(len(DispatchOpcode) - 1)
 input_index_width = unsigned_width(net.num_inputs() - 1)
 spike_width = input_index_width + charge_width(net)
 operand_width = width_nearest_byte(opcode_width + spike_width) - opcode_width
-output_index_width = unsigned_width(net.num_outputs() - 1)
-max_bytes_per_run = (width_bits_to_bytes(opcode_width + output_index_width) *
+max_bytes_per_run = (width_bits_to_bytes(opcode_width + unsigned_width(net.num_outputs() - 1)) *
                      (net.num_outputs() + 1))
 max_runs_ahead = SYSTEM_BUFFER // max_bytes_per_run
 opcode_shift = 8 - opcode_width
