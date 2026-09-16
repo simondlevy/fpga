ac451abc4c5e9c2b82d6363349fdfea92f8d335b
simondlevy <simon.d.levy@gmail.com>
2026-09-16 17:53:04 -0400

    modified:   bs.py

--- tennlab_fpga/posix/bs.py
+++ tennlab_fpga/posix/bs.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 from bitstruct import pack
 from enum import Enum, IntEnum, auto
 
@@ -6,6 +7,9 @@ class DispatchOpcode(IntEnum):
     SPK = auto()
     SNC = auto()
     CLR = auto()
+
+def printclr(fmtstr):
+    print([('x%02X' % c) for c in pack(fmtstr, DispatchOpcode.CLR, 0)])
 
 def printspk(fmtstr, idx, chg):
     print([('x%02X' % c) for c in pack(fmtstr, DispatchOpcode.SPK, idx, chg)])
@@ -16,6 +20,7 @@ def printspk(fmtstr, idx, chg):
 ##_from_fpga.spk_fmt_str:  u2 = opc_width
 ##_from_fpga.cmd_fmt_str:  u2u6 = opc_width,operand_width
 
+printclr('u2u6')
 printspk('u2u1s2', 1, 1)
 
 ## Dronepong:
@@ -24,6 +29,7 @@ printspk('u2u1s2', 1, 1)
 ##_from_fpga.spk_fmt_str:  u2u1
 ##_from_fpga.cmd_fmt_str:  u2u6
 
+printclr('u2u14')
 printspk('u2u1s7', 0, 63)
 
 
