f5fac0a73bf9c9a3b37e201cf57cf16eb3905ff9
simondlevy <simon.d.levy@gmail.com>
2026-09-16 18:07:44 -0400

    modified:   bs.py

--- tennlab_fpga/posix/bs.py
+++ tennlab_fpga/posix/bs.py
@@ -12,6 +12,7 @@ def printclr(fmtstr):
     print([('x%02X' % c) for c in pack(fmtstr, DispatchOpcode.CLR, 0)])
 
 def printspk(fmtstr, idx, chg):
+    print('AS %d %d : ' % (idx, chg), end='')
     print([('x%02X' % c) for c in pack(fmtstr, DispatchOpcode.SPK, idx, chg)])
 
 ## XOR:
@@ -21,6 +22,7 @@ def printspk(fmtstr, idx, chg):
 ##_from_fpga.cmd_fmt_str:  u2u6 = opc_width,operand_width
 
 #printclr('u2u6')
+printspk('u2u1s2', 0, 1)
 printspk('u2u1s2', 1, 1)
 
 ## Dronepong:
@@ -31,11 +33,3 @@ printspk('u2u1s2', 1, 1)
 
 #printclr('u2u14')
 #printspk('u2u1s7', 0, 63)
-
-
-
-
-
-
-
-
