c106a9bd445400abd1d0de225d43ec33250e4f16
Simon D. Levy <simon.d.levy@gmail.com>
2026-09-17 02:53:31 -0400

    modified:   bs.py

--- tennlab_fpga/posix/bs.py
+++ tennlab_fpga/posix/bs.py
@@ -2,6 +2,7 @@
 
 RUN, SPK, SNC, CLR = 0, 1, 2, 3
 
+
 def pack_u(val, size):
     return bin(val)[2:].zfill(size)
 
@@ -13,43 +14,35 @@ def pack_s(val, size):
     return bin(val)[2:].zfill(size)
 
 
+def finish_packing(bit_string):
+
+    # Pad the final sequence with zeros if it doesn't align to an 8-bit byte
+    remainder = len(bit_string) % 8
+    if remainder != 0:
+        bit_string += '0' * (8 - remainder)
+
+    # Group the bits into chunks of 8 and convert them into actual bytes
+    packed_bytes = bytearray()
+    for i in range(0, len(bit_string), 8):
+        byte_chunk = bit_string[i:i+8]
+        packed_bytes.append(int(byte_chunk, 2))
+
+    return bytes(packed_bytes)
+
+
 def pack_spk_command(opcode_width, index_width, charge_width, index, charge):
 
-    bit_string = (
+    return finish_packing(
             pack_u(SPK, opcode_width) +
             pack_u(index, index_width) +
             pack_s(charge, charge_width))
 
-    # Pad the final sequence with zeros if it doesn't align to an 8-bit byte
-    remainder = len(bit_string) % 8
-    if remainder != 0:
-        bit_string += '0' * (8 - remainder)
-
-    # Group the bits into chunks of 8 and convert them into actual bytes
-    packed_bytes = bytearray()
-    for i in range(0, len(bit_string), 8):
-        byte_chunk = bit_string[i:i+8]
-        packed_bytes.append(int(byte_chunk, 2))
-
-    return bytes(packed_bytes)
-
 
 def pack_run_command(opcode_width, operand_width, to_run):
 
-    bit_string = pack_u(RUN, opcode_width) + pack_u(to_run, operand_width)
-
-    # Pad the final sequence with zeros if it doesn't align to an 8-bit byte
-    remainder = len(bit_string) % 8
-    if remainder != 0:
-        bit_string += '0' * (8 - remainder)
-
-    # Group the bits into chunks of 8 and convert them into actual bytes
-    packed_bytes = bytearray()
-    for i in range(0, len(bit_string), 8):
-        byte_chunk = bit_string[i:i+8]
-        packed_bytes.append(int(byte_chunk, 2))
-
-    return bytes(packed_bytes)
+    return finish_packing(
+            pack_u(RUN, opcode_width) +
+            pack_u(to_run, operand_width))
 
 
 result = pack_spk_command(2, 1, 7, 0, 63)
