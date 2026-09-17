e82b6abd68978a5ec45c0f5d98eb456868ca312b
Simon D. Levy <simon.d.levy@gmail.com>
2026-09-16 23:12:02 -0400

    modified:   ../utils/compile.py

--- tennlab_fpga/utils/compile.py
+++ tennlab_fpga/utils/compile.py
@@ -89,12 +89,6 @@ def main():
 
     cpp_code += 'void WriteClr()\n'
     cpp_code += '{\n'
-    cpp_code += ('    const uint8_t bytes[%d] = {0xC0%s};\n' %
-                 (cmd_bytes, (', x00') * (cmd_bytes-1)))
-    cpp_code += '}\n\n'
-
-    cpp_code += 'void WriteSpk()\n'
-    cpp_code += '{\n'
     cpp_code += ('    const uint8_t bytes[%d] = {0x%02X%s};\n' %
                  (cmd_bytes, DispatchOpcode.CLR << (8-opc_width),
                  (', x00') * (cmd_bytes-1)))
