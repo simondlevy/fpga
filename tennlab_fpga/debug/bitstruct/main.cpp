fe5f1a7f85fe9556c1ca78f5790d77ccada5a2da
Simon D. Levy <simon.d.levy@gmail.com>
2026-09-17 22:29:18 -0400

    modified:   main.cpp

--- tennlab_fpga/debug/bitstruct/main.cpp
+++ tennlab_fpga/debug/bitstruct/main.cpp
@@ -2,6 +2,7 @@
 #include <stdio.h>
 
 #include <algorithm>
+#include <iostream>
 #include <vector>
 
 // Bit-twiddling utilities ---------------------------------------------------
@@ -47,6 +48,14 @@ static BitArray zpad(const BitArray inp, const size_t n)
     return append(inp, zeros);
 }
 
+static void Dump(const BitArray bits)
+{
+    for (auto bit : bits) {
+        printf("%d", bit);
+    }
+    printf("\n");
+}
+
 // FPGA stuff ----------------------------------------------------------------
 
 typedef std::vector<uint8_t> ByteArray;
@@ -76,14 +85,17 @@ static ByteArray finish_packing(const BitArray bit_string)
 
     // Group the bits into chunks of 8 and convert them into actual bytes
     auto packed_bytes = ByteArray();
-#if 0
-    for (i in range(0, len(bit_string), 8)) {
-        byte_chunk = bit_string[i:i+8];
-        packed_bytes.append(int(byte_chunk, 2));
-    }
+    for (int i=0; i<bit_string.size(); i += 8) {
 
-    return bytes(packed_bytes);
-#endif
+
+        const auto byte_chunk = BitArray (
+                bit_string.begin() + i,
+                bit_string.begin() + i + 8);
+
+        Dump(byte_chunk);
+
+        //packed_bytes.append(int(byte_chunk, 2));
+    }
 
     return packed_bytes;
 }
@@ -105,14 +117,12 @@ static ByteArray pack_run_command(const int opcode_width, const int operand_widt
 
 int main()
 {
-    for (auto bit : pack_u(3, 6)) {
-        printf("%d", bit);
-    }
-    printf("\n");
+    Dump(pack_u(3, 6));
 
-    pack_spk_command(2, 1, 7, 0, 63);
-    pack_spk_command(2, 1, 7, 1, 63);
-    pack_run_command(2, 6, 3);
+    /*
+       pack_spk_command(2, 1, 7, 0, 63);
+       pack_spk_command(2, 1, 7, 1, 63);
+       pack_run_command(2, 6, 3);*/
 
     return 0;
 }
