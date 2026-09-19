47ebe18084dcef4f54f9ce3900b219beb15f0424
Simon D. Levy <simon.d.levy@gmail.com>
2026-09-19 10:48:46 -0400

    modified:   utils/load.py

--- utils/load.py
+++ utils/load.py
@@ -38,8 +38,8 @@ proc = fpga.Processor(args.target, args.port, args.io_type)
 
 proc.load_network(net, args.spi_flash)
 
-print('to fpga: cmd=%s spk=%s' %
+print('to fpga:   cmd=%-8s spk=%-8s' %
       (proc._to_fpga.cmd_fmt_str,  proc._to_fpga.spk_fmt_str))
 
-print('from fpga: cmd=%s spk=%s' %
+print('from fpga: cmd=%-8s spk=%-8s' %
       (proc._from_fpga.cmd_fmt_str,  proc._from_fpga.spk_fmt_str))
