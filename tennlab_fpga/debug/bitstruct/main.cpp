4eed73de87f8b1858a057c5b1ea386cfbbecd60a
Simon D. Levy <simon.d.levy@gmail.com>
2026-09-18 20:08:03 -0400

    modified:   main.cpp

--- tennlab_fpga/debug/bitstruct/main.cpp
+++ tennlab_fpga/debug/bitstruct/main.cpp
@@ -1,111 +1,7 @@
 #include <stdint.h>
 #include <stdio.h>
+#include <math.h>
 
-#include <algorithm>
-#include <iostream>
-#include <vector>
-
-// Bit-twiddling utilities ---------------------------------------------------
-
-typedef uint8_t Bit;
-
-typedef std::vector<Bit> BitArray;
-
-typedef uint8_t Byte;
-
-typedef std::vector<Byte> ByteArray;
-
-static BitArray Int2Bin(const int val)
-{
-    BitArray reversed_bits;
-
-    auto v = val;
-
-    while (v > 0) {
-        reversed_bits.push_back(v & 0x1);
-        v >>= 1;
-    }
-
-    BitArray bits(reversed_bits.size());
-
-    std::reverse_copy(reversed_bits.begin(), reversed_bits.end(), bits.begin());
-
-    return bits;
-}
-
-static BitArray AppendBits(const BitArray a, const BitArray b)
-{
-    auto c = a;
-
-    c.insert(c.end(), b.begin(), b.end());
-
-    return c;
-}
-
-static BitArray Zpad(const BitArray inp, const size_t n)
-{
-    BitArray zeros;
-
-    while (zeros.size() < n) {
-        zeros.push_back(0);
-    }
-
-    return AppendBits(inp, zeros);
-}
-
-static Byte BitsToByte(const BitArray bits)
-{
-    const Byte byte = 
-        (bits[0] << 7) + 
-        (bits[1] << 6) + 
-        (bits[2] << 5) + 
-        (bits[3] << 4) + 
-        (bits[4] << 3) + 
-        (bits[5] << 2) + 
-        (bits[6] << 1) +
-        bits[7];
-
-    return byte;
-}
-
-static void DumpBits(const BitArray bits)
-{
-    for (auto bit : bits) {
-        printf("%d", bit);
-    }
-    printf("\n");
-}
-
-static void DumpByte(const Byte byte)
-{
-    printf("x%02X ", byte);
-}
-
-static ByteArray BitsToBytes(const BitArray bits)
-{
-    // Pad the final sequence with zeros if it doesn't align to an 8-bit byte
-    const auto remainder = bits.size() % 8;
-    const auto full_bits = remainder == 0 ? bits :
-        Zpad(bits, 8 - remainder);
-
-    // Group the bits into chunks of 8 and convert them into actual bytes
-    auto packed_bytes = ByteArray();
-    for (int i=0; i<full_bits.size(); i += 8) {
-
-        const auto byte_chunk = BitArray (
-                full_bits.begin() + i,
-                full_bits.begin() + i + 8);
-
-        packed_bytes.push_back(BitsToByte(byte_chunk));
-    }
-
-    ByteArray reversed_copy(packed_bytes.rbegin(), packed_bytes.rend());
-
-    return reversed_copy;
-}
-
-
-// FPGA stuff ----------------------------------------------------------------
 
 enum { RUN, SPK, SNC, CLR };
 
@@ -113,43 +9,6 @@ static const int kOpcodeWidth = 2;
 static const int kOperandWidth = 14;
 static const int kIndexWidth = 1;
 static const int kChargeWidth = 7;
-
-static BitArray PackUnsigned(const uint8_t val, const uint8_t size)
-{
-    return Zpad(Int2Bin(val), size-kOpcodeWidth);
-}
-
-/*
-static BitArray PackSigned(const int val, const int size)
-{
-    // Calculate Two's Complement for negative numbers
-    return PackUnsigned(val < 0 ? (1 << size) + val : val, size);
-}*/
-
-
-static ByteArray PackClr()
-{
-    return BitsToBytes(AppendBits(
-                PackUnsigned(CLR, kOpcodeWidth), 
-                PackUnsigned(0, kOperandWidth)));
-}
-
-static ByteArray PackSnc()
-{
-    return BitsToBytes(AppendBits(
-                PackUnsigned(SNC, kOpcodeWidth), 
-                PackUnsigned(0, kOperandWidth)));
-}
-
-static void Dump(const char * label, const ByteArray bytes)
-{
-    printf("%s: ", label);
-    for (auto byte : bytes) {
-        DumpByte(byte);
-    }
-    printf("\n");
-}
-
 
 int main()
 {
@@ -162,8 +21,17 @@ int main()
     SPK 1 63: write: xC0 x6F 
     */
 
-    Dump("CLR", PackClr());
-    Dump("SNC", PackSnc());
+    const uint64_t nbytes = (uint8_t)ceil((kOpcodeWidth + kOperandWidth) / 8);
+
+    uint32_t bits = CLR;
+
+    bits <<= kOperandWidth;
+
+    for (uint8_t k=0; k<nbytes; ++k) {
+        printf("x%02X ", (uint8_t)(bits & 0xFF));
+        bits >>= 8;
+    }
+    printf("\n");
 
     return 0;
 }
