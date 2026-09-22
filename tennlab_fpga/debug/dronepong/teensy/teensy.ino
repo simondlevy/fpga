8097a99df09ba4bee423a52180259e1e76434c0c
Simon D. Levy <simon.d.levy@gmail.com>
2026-09-22 15:05:35 -0400

    new file:   tennlab_fpga/debug/dronepong/teensy/teensy.ino

---
+++ tennlab_fpga/debug/dronepong/teensy/teensy.ino
@@ -0,0 +1,55 @@
+/*
+ * Copyright (c) 2026 Simon D. Levy
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+void setup()
+{
+}
+
+void loop()
+{
+}
+
+#if 0
+#include <stdio.h>
+#include <string>
+
+#include "network_config.h"
+#include <processor.hpp>
+
+#include "spikes.h"
+
+static neuro::Processor proc_;
+
+int main()
+{
+    proc_.Connect();
+
+    int timestep_prev = -1;
+
+    for (auto entry : entries) {
+
+        /*
+        printf("(%d, %d, %d, %d)\n",
+                entry.timestep, entry.id, (int)entry.time, (int)entry.value);*/
+
+        if (timestep_prev != entry.timestep) {
+            if (timestep_prev != -1) {
+                proc_.Run(kSimTime);
+                printf("[%d, %d]\n",
+                        proc_.GetOutputCount(0),  proc_.GetOutputCount(1));
+            }
+            proc_.ClearActivity();
+            timestep_prev = entry.timestep;
+        }
+
+        proc_.ApplySpike(entry.id, entry.time, entry.value);
+    }
+
+    return 0;
+}
+#endif
