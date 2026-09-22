db2b22d3fa82ac4d4eb8198522e07c6f12c70eb0
Simon D. Levy <simon.d.levy@gmail.com>
2026-09-22 12:43:19 -0400

    new file:   tennlab_fpga/examples/*/network_config.h new file:   tennlab_fpga/examples/XorTeensy/network_config.h

---
+++ tennlab_fpga/examples/XorTeensy/network_config.h
@@ -0,0 +1,18 @@
+// AUTO-GENERATED: DO NOT EDIT
+
+#pragma once
+
+#include <processor.hpp>
+
+static const int kOpcodeWidth = 2;
+static const int kInputNeuronIndexWidth = 1;
+static const int kChargeWidth = 2;
+static const int kOperandWidth = 6;
+static const int kOutputNeurons = 1;
+static const int kSpikeValueFactor = 1;
+static const int kOutputNeuronIndexWidth = 0;
+static const int kMaxRunsAhead = 2048;
+static const int kMaxRun = 63;
+static const int kSimTime = 10;
+static const bool kDebug = false;
+
